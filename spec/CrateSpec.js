76360110fccc8c1db2df779497f172ba25fdd936
CodeKrakken <newholm1@hotmail.com>
2021-01-14 16:39:38 +0000

    Update crate's location

--- spec/CrateSpec.js
+++ spec/CrateSpec.js
@@ -11,4 +11,8 @@ describe("Crate", function() {
   it('responds to location', function() {
     expect(crate.location).toBeDefined();
   })
+
+  it('can update its location when moved by robot', function() {
+    expect(crate.update([0,1])).toEqual([0,1])
+  })
 })
