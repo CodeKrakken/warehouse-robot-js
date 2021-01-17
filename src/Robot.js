d011d57f3aed6408aaaec8323010153e096070d2
CodeKrakken <newholm1@hotmail.com>
2021-01-17 16:04:57 +0000

    Refactor input method

--- src/Robot.js
+++ src/Robot.js
@@ -1,37 +1,55 @@
 function Robot() {
   this.location = [0,0]
+  this.directions = [
+    'N',
+    'S',
+    'E',
+    'W',
+    'NW',
+    'NE',
+    'SW',
+    'SE'
+  ]
 }
 
 Robot.prototype.instruct = function(instruction) {
-  switch(instruction) {
-    case 'E':
-      this.try_move([instruction])
-      break
-    case 'W':
-      this.try_move([instruction])
-      break
-    case 'N':
-      this.try_move([instruction])
-      break
-    case 'S':
-      this.try_move([instruction])
-      break
-    case 'NE':
-      this.try_move(['N','E'])
-      break
-    case 'NW':
-      this.try_move(['N','W'])
-      break
-    case 'SE':
-      this.try_move(['S','E'])
-      break
-    case 'SW':
-      this.try_move(['S','W'])
-      break
-    default:
-      return 'Invalid instruction.'      
+  if(this.directions.includes(instruction)) {
+    let instructions = instruction.split('')
+    console.log(instructions)
+    this.try_move(instructions)
+    return this.location
+  } else {
+    return 'Invalid instruction.'
   }
-  return this.location
+  // switch(instruction) {
+  //   case 'E':
+  //     this.try_move([instruction])
+  //     break
+  //   case 'W':
+  //     this.try_move([instruction])
+  //     break
+  //   case 'N':
+  //     this.try_move([instruction])
+  //     break
+  //   case 'S':
+  //     this.try_move([instruction])
+  //     break
+  //   case 'NE':
+  //     this.try_move(['N','E'])
+  //     break
+  //   case 'NW':
+  //     this.try_move(['N','W'])
+  //     break
+  //   case 'SE':
+  //     this.try_move(['S','E'])
+  //     break
+  //   case 'SW':
+  //     this.try_move(['S','W'])
+  //     break
+  //   default:
+  //     return 'Invalid instruction.'      
+  // }
+  
 }
 
 Robot.prototype.try_move = function(directions) {
