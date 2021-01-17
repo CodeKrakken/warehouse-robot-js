73719bb65c351bbc787144655385aa704903b61d
CodeKrakken <newholm1@hotmail.com>
2021-01-17 15:48:53 +0000

    Refactor move function

--- src/Robot.js
+++ src/Robot.js
@@ -5,32 +5,31 @@ function Robot() {
 Robot.prototype.instruct = function(instruction) {
   switch(instruction) {
     case 'E':
-      this.try_move(instruction)
+      this.try_move([instruction])
       break
     case 'W':
-      this.try_move(instruction)
+      this.try_move([instruction])
       break
     case 'N':
-      this.try_move(instruction)
+      this.try_move([instruction])
       break
     case 'S':
-      this.try_move(instruction)
+      this.try_move([instruction])
       break
     case 'NE':
-      this.try_move('N')
-      this.try_move('E')
+      this.try_move(['N','E'])
       break
     case 'NW':
-      this.try_move('N')
-      this.try_move('W')
+      this.try_move(['N'])
+      this.try_move(['W'])
       break
     case 'SE':
-      this.try_move('S')
-      this.try_move('E')
+      this.try_move(['S'])
+      this.try_move(['E'])
       break
     case 'SW':
-      this.try_move('S')
-      this.try_move('W')
+      this.try_move(['S'])
+      this.try_move(['W'])
       break
     default:
       return 'Invalid instruction.'      
@@ -38,11 +37,14 @@ Robot.prototype.instruct = function(instruction) {
   return this.location
 }
 
-Robot.prototype.try_move = function(direction) {
-  if(direction === 'N') { this.location[1]++ }
-  if(direction === 'S') { this.location[1]-- }
-  if(direction === 'E') { this.location[0]++ }
-  if(direction === 'W') { this.location[0]-- }
+Robot.prototype.try_move = function(directions) {
+  let location = this.location 
+  directions.forEach(function(direction) {
+    if(direction === 'N') { location[1]++ }
+    if(direction === 'S') { location[1]-- }
+    if(direction === 'E') { location[0]++ }
+    if(direction === 'W') { location[0]-- }
+  })
 }
 
 module.exports = Robot;
