b90c73df207e1169af6ac8e4de0375c13e562920
CodeKrakken <newholm1@hotmail.com>
2021-01-18 17:35:50 +0000

    Add guard clause for obstructed diagonal southy move

--- src/Robot.js
+++ src/Robot.js
@@ -15,7 +15,7 @@ function Robot() {
 Robot.prototype.instruct = function(instruction) {
   if(this.directions.includes(instruction)) {
     instructions = instruction.split('')
-    this.try_move(instructions)
+    if(this.try_move(instructions) === true) { this.move(instructions) }
     return this.location
   } else {
     return 'Invalid instruction.'
@@ -24,13 +24,24 @@ Robot.prototype.instruct = function(instruction) {
 
 Robot.prototype.try_move = function(directions) {
   let location = this.location
+  let response = true
   directions.forEach(function(direction) {
-    if(direction === 'N' && location[1]+1 <= 5) { location[1]++ }
-    if(direction === 'S' && Math.abs(location[1]-1) <= 5) { location[1]-- }
-    if(direction === 'E' && location[0]+1 <= 5) { location[0]++ }
-    if(direction === 'W' && Math.abs(location[0]-1) <= 5) { location[0]-- }
+    if(direction === 'N' && location[1]+1 > 5) { response = false }
+    if(direction === 'E' && location[0]+1 > 5) { response = false }
+    if(direction === 'S' && Math.abs(location[1]-1) > 5) { response = false }
+    if(direction === 'W' && Math.abs(location[0]-1) > 5) { response = false }
   })
-  
+  return response
+}
+
+Robot.prototype.move = function(directions) {
+  let location = this.location
+  directions.forEach(function(direction) {
+    if(direction === 'N') { location[1]++ }
+    if(direction === 'S') { location[1]-- }
+    if(direction === 'E') { location[0]++ }
+    if(direction === 'W') { location[0]-- }
+  })
 }
 
 module.exports = Robot;
